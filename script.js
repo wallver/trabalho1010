dd6339d6e369bcd02c5b680b517d9e1d6e34a236
Wallace da Silva Francisco <58693179+wallver@users.noreply.github.com>
2023-10-10 20:46:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Altera o texto do título
+document.querySelector("h1").textContent = "Programação e Sistemas deInformação";
+// Adiciona um link para o GitHub
+document.querySelector("footer").innerHTML += `<ahref="https://github.com/bard/explorando-o-mundo-tecnologico">GitHub</a>`;
